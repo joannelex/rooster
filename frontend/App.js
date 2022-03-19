6a97cf871e80fdbe8c083bc64465f1d3b3e52c77
joanne <jlee2002@students.cs.ubc.ca>
2022-03-19 14:31:14 -0700

    adding alarm-ui

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  SafeAreaView,
+  Image, } from 'react-native';
+import React from 'react';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Rooster(:</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>cock-a-</Text>
+
+      <Text style={styles.text}>doodle-do.</Text>
+                    
       <StatusBar style="auto" />
-    </View>
+      <Image 
+        source = {require('./assets/rooster-wakeup.png')} style={styles.image} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'rgba(221, 44, 20, 0.80)',
+    // opacity: 0.87,
+    // alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    width: 250,
+    height: 250,
+    resizeMode: 'contain',
+    alignSelf: 'flex-start',
+
+  },
+  text: {
+    color: 'white', 
+    fontSize: 45, 
+    alignSelf: 'flex-start',
+    paddingLeft: '10%'
+    // paddingTop:'30%'
+  },
+  // text2: {
+  //   color: 'white', 
+  //   fontSize: 45, 
+  //   alignSelf: 'flex-start',
+  //   paddingLeft: '10%'
+  // },
 });
