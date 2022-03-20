acabf8d1e182301a2f1f020845e9a31544309de1
gordonch <gordonch@students.cs.ubc.ca>
2022-03-19 18:23:29 -0700

    Saved route impl

--- backend/routes.py
+++ backend/routes.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify
+import finder
+import model
 
 app = Flask(__name__)
 
 @app.route('/get/<title>/<artist>/<rxnTime>', methods = ['GET'])
 def get_new_music(title, artist, rxnTime):
-    return jsonify({"Title": title,
-                    "Artist": artist})
+    model.put_new_song_info(title, artist, rxnTime)
+    new_title = model.get_new_song_title()
+    new_artist = model.get_new_song_artist()
+    return jsonify({"URL": finder.get_URL(new_title, new_artist),
+                    "Title": new_title,
+                    "Artist": new_artist})
 
 if __name__ == "__main__":
     app.run(debug = True)
