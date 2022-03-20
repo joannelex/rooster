2367b9a3ca35dd5660db127e4cdc8fa2730f654e
gordonch <gordonch@students.cs.ubc.ca>
2022-03-19 18:05:17 -0700

    Implemented GET route and URL finder

--- backend/routes.py
+++ backend/routes.py
@@ -2,9 +2,10 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/get', methods = ['GET'])
-def get_new_music():
-    return jsonify({"Baby": "Justin Bieber"})
+@app.route('/get/<title>/<artist>/<rxnTime>', methods = ['GET'])
+def get_new_music(title, artist, rxnTime):
+    return jsonify({"Title": title,
+                    "Artist": artist})
 
 if __name__ == "__main__":
     app.run(debug = True)
